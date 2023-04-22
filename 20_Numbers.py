a1067a05693d9d308494fa34ad632784e469b008
unknown <arixxdelera21@gmail.com>
2023-04-22 16:49:51 +0800

    adding some designs

--- 20_Numbers.py
+++ 20_Numbers.py
@@ -1,8 +1,20 @@
 # Delera, Aritz B.
 
-# Write a Python program that reads a text file named numbers.txt that contains 20 integers. The program will create two other text file; 
-# the first text file will be named even.txt that will contains all even numbers extracted from the numbers.txt. 
-# The second text file will be named odd.txt that will contains all odd numbers extracted from the numbers.txt.
+import pyfiglet
+import random
+
+opening = pyfiglet.figlet_format("= O.O.P =", font = "lean")
+print(opening)
+
+
+# Create an introduction
+print("=" * 61)
+print(" Welcome to AritzMetic's Number Sorter! ".center(60, "+"))
+print("=" * 61)
+
+ # Ask the name of the user to create a greeting
+name = input("\033[30mHi Smart Pipol! what is your name?: \033[0m")
+print("\033[40mHi", name, "! AritzMetic is here to help you in Sorting 20 Numbers!\033[0m")
 
 # Open the numbers.txt 
 with open("numbers.txt", "r") as input_file:
@@ -18,12 +30,28 @@ for num in numbers:
         even_numbers.append(num)
     else:
         odd_numbers.append(num)
+
+print(f"\033[46mIn the list, We found {len(even_numbers)} even numbers and {len(odd_numbers)} odd numbers!\033[0m")
+
+# Shuffle the even and odd numbers at random.
+random.shuffle(even_numbers)
+random.shuffle(odd_numbers)
+
 # Create the file even.txt and write all of the even integers that were extracted.
 with open("even.txt", "w") as even_file:
     for n in even_numbers:
+        even_file.write("We collected a list of all even numbers:\n")
         even_file.write(str(n) + "\n")
+
+print("\033[31mWe created a new file named 'even.txt' and printed out the even integers there.\033[0m")
 
 # Create the file oo.txt and write all of the odd integers that were extracted.
 with open("odd.txt", "w") as odd_file:
     for n in odd_numbers:
+        odd_file.write("We collected a list of all odd numbers:\n")
         odd_file.write(str(n) + "\n")
+
+print("\033[31mWe created a new file named 'odd.txt' and printed out the odd integers there.\033[0m")
+
+closing = pyfiglet.figlet_format("Thank you for utilizing Aritzmetic's Number Sorter! We hope that the information was helpful to you.", font="puffy")
+print(closing)
